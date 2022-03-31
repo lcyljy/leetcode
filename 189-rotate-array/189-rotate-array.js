538da8e1f025984e1a52501455ac13bb5c4c1931
ChangYoung Lee <lodin_love@naver.com>
2022-03-31 09:26:20 +0900

    Time: 138 ms (44.42%), Space: 54.9 MB (6.97%) - LeetHub

--- 189-rotate-array/189-rotate-array.js
+++ 189-rotate-array/189-rotate-array.js
@@ -5,29 +5,29 @@
  */
 var rotate = function(nums, k) {
 //     nums 배열이 주어지고, k만큼 오른쪽으로 민다.
-//     첫번째 시도 - Time Limit Exceeded
-//     if (k>=nums.length){
-//         k = k % nums.length;
-//     }
-//     for(let i=0; i<k; i++){
-//         let lastArr = nums[nums.length-1]
-//         nums.pop();
-//         nums.unshift(lastArr)
-//     }
-//     return nums
+    // 첫번째 시도 - Time Limit Exceeded
+    // if (k>=nums.length){
+    //     k = k % nums.length;
+    // }
+    // for(let i=0; i<k; i++){
+    //     let lastArr = nums[nums.length-1]
+    //     nums.pop();
+    //     nums.unshift(lastArr)
+    // }
+    // return nums
     
 //     두번째 시도 - Wrong Answer
-//     if (k >= nums.length){
-//         k = k % nums.length;
-//     }
-//     if( k == 0 ){
-//         return nums
-//     }
-//     let a = [];
-//     let b = [];
-//     a = nums.slice(-k);
-//     b = nums.slice(0,nums.length-k);
-//     nums = a.concat(b);
+    // if (k >= nums.length){
+    //     k = k % nums.length;
+    // }
+    // if( k == 0 ){
+    //     return nums
+    // }
+    // let a = [];
+    // let b = [];
+    // a = nums.slice(-k);
+    // b = nums.slice(0,nums.length-k);
+    // nums = a.concat(b);
 //     return nums;
     
 //     3번째 트라이... - Wrong Answer
@@ -41,7 +41,7 @@ var rotate = function(nums, k) {
 //     nums.length = length;
 //     return nums;
     
-    
+    // 4번째 트라이 - time Limit Exceeded
 //         if (k>=nums.length){
 //         k = k % nums.length;
 //     }
@@ -51,10 +51,31 @@ var rotate = function(nums, k) {
         
 //     }
 //     return nums
-    if( k >= nums.length ){
+    
+    // 5번째 트라이 - pass
+    // if( k >= nums.length ){
+    //     k = k % nums.length;
+    // }
+    // const a = nums.splice(nums.length - k, k)
+    // console.log(a);
+    // nums.unshift(...a);
+    
+    // 6번째 트라이 - time Limit Exceeded
+//         if (k>=nums.length){
+//         k = k % nums.length;
+//     }
+//     for(let i=0; i<k; i++){
+//         const temp = nums.pop()
+//         nums.unshift(temp);
+        
+//     }
+     // 7번째 트라이 - 3트 변형 - Wrong Answer
+    if( k >= nums.length){
         k = k % nums.length;
     }
-    const a = nums.splice(nums.length - k, k)
-    console.log(a);
+    let length = nums.length;
+    let a = nums.slice(-k);
     nums.unshift(...a);
+    nums.length = length;
+
 };
